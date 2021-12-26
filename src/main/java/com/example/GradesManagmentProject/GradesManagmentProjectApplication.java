a57ed7997bc74ee45484fd6ec5623491cede756c
melinahajdini <61281674+melinahajdini@users.noreply.github.com>
2021-12-26 21:00:01 +0100

    Initial commit

---
+++ src/main/java/com/example/GradesManagmentProject/GradesManagmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.GradesManagmentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesManagmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesManagmentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GradesManagmentProject/GradesManagmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GradesManagmentProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradesManagmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
