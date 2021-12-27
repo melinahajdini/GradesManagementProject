89d870bc2c307513860abe2843d8c3e44c3a898e
melinahajdini <61281674+melinahajdini@users.noreply.github.com>
2021-12-27 18:24:04 +0100

    Pass model and add register form view

--- src/main/java/com/example/GradesManagmentProject/Controller/AuthController.java
+++ src/main/java/com/example/GradesManagmentProject/Controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.GradesManagmentProject.Controller;
 
+import com.example.GradesManagmentProject.Model.ProfessorModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/grades")
 public class AuthController {
     @GetMapping("/register-professor")
-    public String getRegister(){
+    public String getRegister(Model model){
+        model.addAttribute("professorModel", new ProfessorModel());
         return "register";
     }
 }
