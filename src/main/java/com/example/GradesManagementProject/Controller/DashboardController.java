6188481990f9d2a6d53504b18473df6f42ba3a1c
melinahajdini <61281674+melinahajdini@users.noreply.github.com>
2021-12-29 01:44:38 +0100

    Add student in database

--- src/main/java/com/example/GradesManagementProject/Controller/DashboardController.java
+++ src/main/java/com/example/GradesManagementProject/Controller/DashboardController.java
@@ -1,14 +1,41 @@
 package com.example.GradesManagementProject.Controller;
 
+import com.example.GradesManagementProject.Model.ProfessorModel;
+import com.example.GradesManagementProject.Model.Student;
+import com.example.GradesManagementProject.Repository.StudentRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/grades")
 public class DashboardController {
+    private final StudentRepository studentRepository;
+
+    public DashboardController(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     @GetMapping("/dashboard")
     public String getDashboard(){
         return "dashboard";
     }
+    @GetMapping("/student")
+    public String getStudent(Model model){
+        model.addAttribute("student", new Student());
+        return "student";
+
+    }
+    @PostMapping("/student")
+    public String postStudent(Model model, @ModelAttribute Student student){
+            model.addAttribute("student", student);
+
+
+        this.studentRepository.save(student);
+        return "dashboard";
+    }
+
 }
